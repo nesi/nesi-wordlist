eecd1035620dc81452809daafaf54550c0d71bf4
cal <callum.walley@nesi.org.nz>
2023-12-04 12:00:37 +1300

    Added make software names list, and output

---
+++ make_software_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.9
+
+from ruamel.yaml import YAML
+import urllib.request
+import json
+
+OUTFILE = "dic/software_names.yaml"
+LISTURL = "https://raw.githubusercontent.com/nesi/modules-list/main/module-list.json"
+
+
+def main():
+    outdata = {}
+    with urllib.request.urlopen(LISTURL) as url:
+        data = json.load(url)
+        for key, value in data.items():
+            outdata[key] = {"long": value["description"], "possessive": True}
+        yaml = YAML()
+        yaml.dump(outdata, open(OUTFILE, "w", encoding="utf-8"))
+
+
+main()
